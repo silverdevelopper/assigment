f568d6cfd606eae447074a466b546a1b3e5df258
silverdevelopper <mng2147@gmail.com>
2020-02-05 13:39:57 +0300

    cenqieme

---
+++ target/rls/debug/deps/hello_rust-3792461bee34e59c.d
@@ -0,0 +1,5 @@
+/Users/mac/Desktop/hello-rust/target/rls/debug/deps/hello_rust-3792461bee34e59c.rmeta: src/main.rs
+
+/Users/mac/Desktop/hello-rust/target/rls/debug/deps/hello_rust-3792461bee34e59c.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/hello_rust-ab8f456f0003dcf6.d
@@ -0,0 +1,5 @@
+/Users/mac/Desktop/hello-rust/target/rls/debug/deps/hello_rust-ab8f456f0003dcf6.rmeta: src/main.rs
+
+/Users/mac/Desktop/hello-rust/target/rls/debug/deps/hello_rust-ab8f456f0003dcf6.d: src/main.rs
+
+src/main.rs:
